eb633508163c92d44b66e343f84b6bb9f87a17eb
Ilhomjon Abdusaminov <abdusaminovilhomjon30037007@gmail.com>
2022-03-28 22:06:55 +0500

    Bug Fixed

---
+++ ApiTest/Migrations/20220328170606_ThirtyMig.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ApiTest.Migrations
+{
+    public partial class ThirtyMig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
